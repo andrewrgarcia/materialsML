3bea5961dc39c480b447a302065c4abd24ce16a8
andrewrgarcia <garcia.gtr@gmail.com>
2023-01-15 00:36:51 -0500

    foundation for high level concepts: iterative loading of properties for multiple materials with Crate class, consider making a Decorator with conditionals inside the same class

--- materialsml/main.py
+++ materialsml/main.py
@@ -2,7 +2,7 @@ from mp_api.client import MPRester
 from pymatgen.analysis.diffraction.xrd import XRDCalculator
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
-# from torch_geometric.data import HeteroData
+mendeleev_elements = {'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9, 'Ne': 10, 'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17, 'Ar': 18, 'K': 19, 'Ca': 20, 'Sc': 21, 'Ti': 22, 'V': 23, 'Cr': 24, 'Mn': 25, 'Fe': 26, 'Co': 27, 'Ni': 28, 'Cu': 29, 'Zn': 30, 'Ga': 31, 'Ge': 32, 'As': 33, 'Se': 34, 'Br': 35, 'Kr': 36, 'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40, 'Nb': 41, 'Mo': 42, 'Tc': 43, 'Ru': 44, 'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50, 'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57, 'Ce': 58, 'Pr': 59, 'Nd': 60, 'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 'Tb': 65, 'Dy': 66, 'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 'Hf': 72, 'Ta': 73, 'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79, 'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83, 'Po': 84, 'At': 85, 'Rn': 86, 'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90, 'Pa': 91, 'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98, 'Es': 99, 'Fm': 100, 'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105, 'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109, 'Ds': 110, 'Rg': 111, 'Cn': 112, 'Nh': 113, 'Fl': 114, 'Mc': 115, 'Lv': 116, 'Ts': 117, 'Og': 118}
 
 def load(API_KEY, MATERIAL_ID, conventional=True):
     '''helper function to load structural information of Material
@@ -80,41 +80,77 @@ class Solid:
         '''
         structure = Solid(self.API_KEY, self.MATERIAL_ID).load(conventional=True)
 
-        elements = {'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9, 'Ne': 10, 'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17, 'Ar': 18, 'K': 19, 'Ca': 20, 'Sc': 21, 'Ti': 22, 'V': 23, 'Cr': 24, 'Mn': 25, 'Fe': 26, 'Co': 27, 'Ni': 28, 'Cu': 29, 'Zn': 30, 'Ga': 31, 'Ge': 32, 'As': 33, 'Se': 34, 'Br': 35, 'Kr': 36, 'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40, 'Nb': 41, 'Mo': 42, 'Tc': 43, 'Ru': 44, 'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50, 'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57, 'Ce': 58, 'Pr': 59, 'Nd': 60, 'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 'Tb': 65, 'Dy': 66, 'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 'Hf': 72, 'Ta': 73, 'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79, 'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83, 'Po': 84, 'At': 85, 'Rn': 86, 'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90, 'Pa': 91, 'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98, 'Es': 99, 'Fm': 100, 'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105, 'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109, 'Ds': 110, 'Rg': 111, 'Cn': 112, 'Nh': 113, 'Fl': 114, 'Mc': 115, 'Lv': 116, 'Ts': 117, 'Og': 118}
-
         Nsites = len(structure.sites)
-
-        # nodes = HeteroData()
 
         nodes = {}
         ckeys = ['x','y','z']
         for i in ['atomic_number',*ckeys]:
             nodes[i] = [] 
 
-        if not fractional:
-            for i in range(Nsites):
-                # atomic_num = element(str(structure.sites[i].specie)).atomic_number # specie is not a typo
-                atomic_num = elements[str(structure.sites[i].specie)] # specie is not a typo
 
-                # nodes['atomic_number'].x.append(atomic_num)
-                # [ nodes[ckeys[k]].x.append(structure.sites[i].coords[k]) for k in range(3) ]
+        for i in range(Nsites):
+            # atomic_num = element(str(structure.sites[i].specie)).atomic_number # specie is not a typo
+            atomic_num = mendeleev_elements[str(structure.sites[i].specie)] # specie is not a typo
 
-                nodes['atomic_number'].append(atomic_num)
-                [ nodes[ckeys[k]].append(structure.sites[i].coords[k]) for k in range(3) ]
+            nodes['atomic_number'].append(atomic_num)
+            [ nodes[ckeys[k]].append(structure.sites[i].coords[k]) if not fractional else \
+              nodes[ckeys[k]].append(structure.sites[i].frac_coords[k]) for k in range(3) ]
 
-        else:
-            for i in range(Nsites):
-                # atomic_num = element(str(structure.sites[i].specie)).atomic_number 
-                atomic_num = elements[str(structure.sites[i].specie)] # specie is not a typo
+        self.graph = nodes
 
-                # nodes['atomic_number'].append(atomic_num)
-                # [ nodes[ckeys[k]].x.append(structure.sites[i].frac_coords[k]) for k in range(3) ]
+    def add_factor(self, key, value):
 
-                nodes['atomic_number'].append(atomic_num)
-                [ nodes[ckeys[k]].append(structure.sites[i].frac_coords[k]) for k in range(3) ]
-
-        return nodes
+        self.graph[key] = value
 
 
+class Crate:
+    def __init__( self, API_KEY, MATERIALS = 'mp-1103503'):
+        '''Material class. To store and process material data
 
+        Parameters
+        ----------
+        API_KEY : str
+            secret API KEY used to run MPRester data requests
+        MATERIAL_ID : int
+            ID of selected material (without "mp-" prefix)
+        graph : object
+            Heterogeneous graph to store all information from a `Solid` material
+        '''
+        self.API_KEY = API_KEY
+        self.MATERIALS = MATERIALS
+        self.graphs = {}
+        self.blacklist = []
 
+    def addFeature(func):
+        def wrapper(self):
+
+            for i in self.MATERIALS:
+                if i not in self.blacklist:
+                    if self.graphs.get(i) == None:
+                        self.graphs.update({ i: {} })
+                
+                x,y = func(i)
+
+                self.graphs[i].update({ y: x })
+
+        return wrapper
+
+# @addFeature
+# def foo(x):
+#     return x+1
+'example / model'
+@Crate.addFeature
+def rester(x):
+    with MPRester(api_key='<enter your api key>') as mpr:
+
+        # for a single material
+        # thermo_doc = mpr.thermo.get_data_by_id('mp-1103503')
+        # thermo_doc = mpr.thermo.get_data_by_id(x)
+
+        # for many materials, it's much faster to use
+        # the `search` method, where additional material_ids can 
+        # be added to this list
+        # thermo_docs = mpr.thermo.search(material_ids=['mp-1103503'])
+        thermo_docs = mpr.thermo.search(material_ids=[x])
+
+    return thermo_docs, 'thermo_label'
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,13 +1,30 @@
 import materialsml as mml
+from mp_api.client import MPRester as mpr
+import pandas as pd
 
 # replace below line with API key [ in string form ] from Materials Project site (https://materialsproject.org/api#api-key)
-# SECRET_KEY = []
+SECRET_KEY = ''    
 
 def test_materialprops():
+    # material = mml.Solid(SECRET_KEY, 'mp-1103503')
     material = mml.Solid(mml.SECRET_KEY, 'mp-1103503')
 
-    b = material.topology()
-    print(b)
+    # material.topology()
+    # print(material.graph)
 
+    material.add_factor('poo',10203)
 
+    print(material.graph)
 
+# def test_wrapper():
+    
+#     material = mml.Solid(mml.SECRET_KEY, 'mp-1103503')
+
+#     @material.call      
+#     def get(material_id):  
+#         return mpr.get_charge_density_from_material_id(material_id=material_id)
+
+#     print(get('mp-1103503' ))
+
+    
+    
