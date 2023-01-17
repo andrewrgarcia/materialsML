d55ebcccfa1cbeb0ece2d4f2fc2d311154218994
andrewrgarcia <garcia.gtr@gmail.com>
2023-01-17 13:43:44 -0500

    lean and fast method for bulk properties search & add queryAdd()

--- materialsml/main.py
+++ materialsml/main.py
@@ -17,7 +17,6 @@ import json
 
 mendeleev_elements = {'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9, 'Ne': 10, 'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17, 'Ar': 18, 'K': 19, 'Ca': 20, 'Sc': 21, 'Ti': 22, 'V': 23, 'Cr': 24, 'Mn': 25, 'Fe': 26, 'Co': 27, 'Ni': 28, 'Cu': 29, 'Zn': 30, 'Ga': 31, 'Ge': 32, 'As': 33, 'Se': 34, 'Br': 35, 'Kr': 36, 'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40, 'Nb': 41, 'Mo': 42, 'Tc': 43, 'Ru': 44, 'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50, 'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57, 'Ce': 58, 'Pr': 59, 'Nd': 60, 'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 'Tb': 65, 'Dy': 66, 'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 'Hf': 72, 'Ta': 73, 'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79, 'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83, 'Po': 84, 'At': 85, 'Rn': 86, 'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90, 'Pa': 91, 'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98, 'Es': 99, 'Fm': 100, 'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105, 'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109, 'Ds': 110, 'Rg': 111, 'Cn': 112, 'Nh': 113, 'Fl': 114, 'Mc': 115, 'Lv': 116, 'Ts': 117, 'Og': 118}
 
-
 def save_to_json(filename="graphs.json",class_object={}):
     '''Save object to JSON file
     adapted from: https://pythonspot.com/save-a-dictionary-to-a-file/
@@ -26,11 +25,11 @@ def save_to_json(filename="graphs.json",class_object={}):
     # python dictionary with key value pairs
     dict = class_object
     # create json object from dictionary
-    json = json.dumps(dict)
+    jsonobj = json.dumps(dict)
     # open file for writing, "w" 
     f = open(filename,"w")
     # write json object to file
-    f.write(json)
+    f.write(jsonobj)
     # close file
     f.close()
 
@@ -39,6 +38,46 @@ def load_from_json(filename="graphs.json"):
     with open(filename) as f:
         data = f.read()
     return json.loads(data)
+
+
+
+def topol(structure, fractional=False, conventional=True):
+    '''processes the material topology as a hashmap with an a-xyz coordinate format 
+    i.e. [atom_numbers, x_coord, y_coord, z_coord]
+    from a MPR[material-id].summary.structure object
+
+    Parameters
+    ----------
+    fractional: bool
+        if False, returns xyz coordinates else returns fractional (a,b,c) coordinates
+    conventional: bool
+        if True, returns conventional standard structure else returns primitive
+    '''
+    # important to use the conventional structure to ensure
+    # that peaks are labelled with the conventional Miller indices
+    sga = SpacegroupAnalyzer(structure)
+
+    if conventional:
+        structure = sga.get_conventional_standard_structure()
+
+    Nsites = len(structure.sites)
+
+    nodes = {}
+    ckeys = ['x','y','z']
+    for i in ['atomic_number',*ckeys]:
+        nodes[i] = [] 
+
+
+    for i in range(Nsites):
+        # atomic_num = element(str(structure.sites[i].specie)).atomic_number # specie is not a typo
+        atomic_num = mendeleev_elements[str(structure.sites[i].specie)] # specie is not a typo
+
+        nodes['atomic_number'].append(atomic_num)
+        [ nodes[ckeys[k]].append(structure.sites[i].coords[k]) if not fractional else \
+            nodes[ckeys[k]].append(structure.sites[i].frac_coords[k]) for k in range(3) ]
+
+    return nodes
+
 
 
 class Solid:
@@ -68,60 +107,24 @@ class Solid:
         '''
         self.graph = load_from_json(filename)
 
-    def structure_loader(self, conventional=True):
+    def topology(self, fractional=False, conventional=True):
         '''helper function to load structural information of Material
-
-        Parameters
-        ----------
-        conventional: bool
-            if True, returns conventional standard structure else returns primitive
-        '''
-        with MPRester(self.API_KEY) as mpr:
-
-            # first retrieve the relevant structure
-            structure = mpr.get_structure_by_material_id(self.MATERIAL_ID)
-        
-        # important to use the conventional structure to ensure
-        # that peaks are labelled with the conventional Miller indices
-        sga = SpacegroupAnalyzer(structure)
-        conventional_structure = sga.get_conventional_standard_structure()
-        
-        if conventional:
-            structure = conventional_structure
-
-        return structure
-
-
-    def topology(self, fractional=False):
-        '''processes the material topology as a hashmap with an a-xyz coordinate format 
-        i.e. [atom_numbers, x_coord, y_coord, z_coord]
 
         Parameters
         ----------
         fractional: bool
             if False, returns xyz coordinates else returns fractional (a,b,c) coordinates
+        conventional: bool
+            if True, returns conventional standard structure else returns primitive
         '''
-        structure = Solid(self.API_KEY, self.MATERIAL_ID).structure_loader(conventional=True)
-
-        Nsites = len(structure.sites)
-
-        nodes = {}
-        ckeys = ['x','y','z']
-        for i in ['atomic_number',*ckeys]:
-            nodes[i] = [] 
+        with MPRester(self.API_KEY) as mpr:
+            # first retrieve the relevant structure
+            structure = mpr.get_structure_by_material_id(self.MATERIAL_ID)
+        
+        self.graph = topol(structure, fractional, conventional=True)
 
 
-        for i in range(Nsites):
-            # atomic_num = element(str(structure.sites[i].specie)).atomic_number # specie is not a typo
-            atomic_num = mendeleev_elements[str(structure.sites[i].specie)] # specie is not a typo
-
-            nodes['atomic_number'].append(atomic_num)
-            [ nodes[ckeys[k]].append(structure.sites[i].coords[k]) if not fractional else \
-              nodes[ckeys[k]].append(structure.sites[i].frac_coords[k]) for k in range(3) ]
-
-        self.graph = nodes
-
-    def addProperty(self, key, value):
+    def add(self, key, value):
         '''Add a specific property label and its corresponding value or set of values. 
 
         Parameters
@@ -157,7 +160,6 @@ class Crate:
         self.API_KEY = API_KEY
         self.MATERIALS = []
         self.graphs = {}
-        self.blacklist = []
         self.num_cores = 1
 
     def save(self,filename="graphs.json"):
@@ -170,64 +172,29 @@ class Crate:
         '''
         self.graphs = load_from_json(filename)
 
-    def topology(self, fractional=False):
-        '''Same function as the Materials().topology() method but iterative, with the list of 
-        specified material IDs in Crate().MATERIALS
+
+    def queryAdd(self,*args):
         '''
-        for i in self.MATERIALS:
-            if i not in self.blacklist:
-                if self.graphs.get(i) == None:
-                    self.graphs.update({ i: {} })
-
-                material = Solid(self.API_KEY,i)
-                try:
-                    material.topology()
-                except:
-                    pass
-
-                if material.graph:
-                    [self.graphs[i].update({ j: material.graph[j] }) for j in material.graph.keys()]
-                else:
-                    self.graphs.pop(i)
-                    self.blacklist.append(i)
-
-
-    def addProperty(self,func):
-        '''Decorator used to add several properties iteratively from a REST function
-
-        Usage Example:
-        -----------------------
-        materials_box = Crate(MY_SECRET_KEY)
-
-        @materials_box.addProperty
-        def REST_function_mag(CRATE):      
-            with MPRester(api_key=MY_SECRET_KEY) as mpr:
-                magnetism_doc = mpr.magnetism.get_data_by_id(CRATE)
-
-            return magnetism_doc.num_unique_magnetic_sites, 'num_sites'
+        Example Usage:
+        queryAdd("structure","total_magnetization")
+        '''
+        print(list(args)[0])
+        args_pass = list(args)[0]
+        with MPRester(api_key=self.API_KEY) as mpr:
+            mpresults = mpr.summary.search(material_ids=self.MATERIALS, fields=['material_id',*args_pass])
+    
         
-        REST_function_mag(materials_box)      
-        '''
-        def wrapper(self):
-            for i in self.MATERIALS:
-                if i not in self.blacklist:
-                    if self.graphs.get(i) == None:
-                        self.graphs.update({ i: {} })
-                    'try if entries for the i material exist else eliminate material from dataset and put on blacklist'
-                    x,y = None,0      # patch for "UnboundLocalError: local variable 'x' referenced before assignment"
-                    try:
-                        x,y = func(i)
-                    except:
-                        pass
-                    # 'above strategy results in brownout because first function will overload tasks if does not exist'
-                    # x,y = func(i)
-                    if x is not None:
-                        self.graphs[i].update({ y: x })
-                    else:
-                        self.graphs.pop(i)
-                        self.blacklist.append(i)
-
-        return wrapper
+        for i in mpresults:
+            mid = i.material_id
+            self.graphs.update({ mid : {} })
+            for j in args_pass:
+                if j == 'structure':
+                    topol_nodes = topol(getattr(i,j), fractional=False, conventional=True)
+                    for k in ["atomic_number",*list('xyz')]:
+                        self.graphs[mid].update({ k : topol_nodes[k]})
+                else:
+                    print(j)
+                    self.graphs[mid].update({j : getattr(i,j)})
 
     def process(self,label='num_sites'):
         '''Process graphs for machine learning. 
@@ -262,8 +229,17 @@ class Network:
         self.epochs = 500
         self.fitted_model = []
         self.test_gen = []
+
+    def save(self,path='path/to/location'):
+        
+        self.fitted_model.save()
+
+    def load(self,path='path/to/location'):
+
+        model = keras.models.load_model('path/to/location')
+        self.fitted_model = model
     
-    def desc(self):
+    def description(self):
         summary = pandas.DataFrame(
         [(g.number_of_nodes(), g.number_of_edges()) for g in self.graphs],
         columns=["nodes", "edges"],
@@ -302,16 +278,6 @@ class Network:
         self.fitted_model = model
 
         sg.utils.plot_history(history) if plot else None
-
-    def save(self,path='path/to/location'):
-        
-        self.fitted_model.save()
-
-    def load(self,path='path/to/location'):
-
-        model = keras.models.load_model('path/to/location')
-        self.fitted_model = model
-
 
     def test(self): return self.fitted_model.evaluate(self.test_gen)
 
--- tests/test_module.py
+++ tests/test_module.py
@@ -11,86 +11,16 @@ def test_materialprops():
     material.topology()
     print(material.graph)
 
-    material.addProperty('poo',10203)
+    material.add('yoo',10203)
 
     print(material.graph)
 
 
 def test_multiREST():
     crate = mml.Crate(mml.SECRET_KEY)
-
-    @crate.addProperty
-    def bandgap(CRATE):
-        with MPRester(api_key=mml.SECRET_KEY) as mpr:
-            try:
-                bandstructure = mpr.get_bandstructure_by_material_id(CRATE,line_mode=False)
-            except:
-                pass
-            feature = bandstructure.get_band_gap()['energy'] if bandstructure else None
-        return feature, 'band_gap_energy'
-
-    @crate.addProperty
-    def totalmag(CRATE):      
-        '''magnetic properties data for Material'''
-        with MPRester(api_key=mml.SECRET_KEY) as mpr:
-            try:
-                magnetism_doc = mpr.magnetism.get_data_by_id(CRATE)
-            except:
-                pass
-            feature = magnetism_doc.total_magnetization if magnetism_doc else None
-        return feature, 'total magnetization'
-
-    @crate.addProperty
-    def magmoms(CRATE):      
-        '''magnetic properties data for Material'''
-        with MPRester(api_key=mml.SECRET_KEY) as mpr:
-            try:
-                magnetism_doc = mpr.magnetism.get_data_by_id(CRATE)
-            except:
-                pass
-            feature = magnetism_doc.magmoms if magnetism_doc else None 
-        return feature, 'mag. moments'
-
-    @crate.addProperty
-    def num_magsites(CRATE):      
-        '''magnetic properties data for Material'''
-        with MPRester(api_key=mml.SECRET_KEY) as mpr:
-            try:
-                magnetism_doc = mpr.magnetism.get_data_by_id(CRATE)
-            except:
-                pass
-            feature = magnetism_doc.num_unique_magnetic_sites if magnetism_doc else None
-        return feature, 'num unique mag. sites'
-
-    @crate.addProperty
-    def thermo(CRATE):
-
-        with MPRester(api_key=mml.SECRET_KEY) as mpr:
-            # # for a single material
-            # thermo_doc = mpr.thermo.get_data_by_id('mp-1103503')
-
-            # # for many materials, it's much faster to use
-            # # the `search` method, where additional material_ids can 
-            # # be added to this list
-            try:
-                thermo_docs = mpr.thermo.search(material_ids=[CRATE])
-            except:
-                pass
-            feature = thermo_docs[0].formation_energy_per_atom if thermo_docs else None
-        return feature, 'form. energy'
-
-    crate.MATERIALS = ['mp-'+str(i) for i in range(1103500,1103530)]
-
-    print(crate.MATERIALS)
-
-    crate.topology()
-
-    bandgap(crate)
-    totalmag(crate)
-    # magmoms(crate) 
-    # num_magsites(crate)      
-    # thermo(crate)
-
+    crate.MATERIALS =  ['mp-'+str(i) for i in range(10)]
+    crate.queryAdd(["structure","total_magnetization","band_gap"])
 
     print(crate.graphs)
-    print(crate.blacklist)
+
+    crate.save()
